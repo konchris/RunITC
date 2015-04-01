15f946a4e034465c2d5411c4fa41ea70f764b98c
Christopher Espy <github@konchris.de>
2015-04-01 06:31:50 +0200

    Created the first test, which fails.

---
+++ RunITC/test/test_runitc.py
@@ -0,0 +1,14 @@
+import unittest
+
+from RunITC import ITCDevice
+
+
+class DeviceTestCase(unittest.TestCase):
+    """Test the device class."""
+
+    def test_device_class_is_present(self):
+        """If something something."""
+        self.assertEqual(True, True)
+
+if __name__ == "__main__":
+    unittest.main()
