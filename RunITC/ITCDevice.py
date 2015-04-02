76c4152391e3422ce603c19be11af3881ac0cc73
Christopher Espy <github@konchris.de>
2015-04-02 23:12:44 +0200

    Made progress on getting much of the itc functionality coded and tested.

--- RunITC/ITCDevice.py
+++ RunITC/ITCDevice.py
@@ -1,21 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 """The ITC Module.
 
 """
+
 import visa
+
+DEVPATH = '/home/chris/Programming/github/RunITC/test/devices.yaml'
 
 
 class ITCDevice(object):
 
-    def __init__(self):
+    def __init__(self, address="GPIB1::24::INSTR", read_term="\r",
+                 write_term="\r"):
         super(ITCDevice, self).__init__()
         self.resource = None
+        self.address = address
+        self.read_term = read_term
+        self.write_term = write_term
+        self.heater_set = False
+        self.auto_heat = False
+
+    def set_resource(self, resource):
+        self.resource = resource(self.address,
+                                 read_termination=self.read_term,
+                                 write_termination=self.write_term)
+
+    def get_tsorp(self):
+        "Get the temperature at the sorption pump."
+        return self.resource.query("R1")
+
+    def get_the3(self):
+        "Get the temperature at the sorption pump."
+        return self.resource.query("R2")
+
+    def get_t1k(self):
+        "Get the temperature at the sorption pump."
+        return self.resource.query("R3")
+
+    def _set_heater_to_tsrop(self):
+        "Set the heater to the sorption pump"
+        self.resource.query("H1")
+        self.heater_set = True
+
+    def get_heater_sensor(self):
+        "Get the heater sensor"
+        sensor_nr = self.resource.query("X")
+        if sensor_nr == 'ERROR':
+            sensor_nr = self.resource.query("XH")
+        return sensor_nr.split("H")[-1][0]
+
+    def set_setpoint(self, setpoint):
+        "Set the set point temperature of the sorption pump"
+        if not isinstance(setpoint, (float, int)):
+            raise TypeError("The setpoint provided needs to be a float "
+                            "or an int.")
+        if not self.heater_set:
+            self._set_heater_to_tsrop()
+        self.resource.query("T{:.3f}".format(setpoint))
+
+    def get_setpoint(self):
+        "Get the setpoint of the sorption pump heater."
+        return self.resource.query("R0")
+
+    def auto_heat_on(self):
+        "Turn on the auto heat control."
+        self.resource.query("A1")
+        self.auto_heat = True
+
+    def auto_heat_off(self):
+        "Turn on the auto heat control."
+        self.resource.query("A0")
+        self.auto_heat = False
+
+    def get_auto_heat_status(self):
+        sensor_nr = self.resource.query("X")
+        if sensor_nr == 'ERROR':
+            sensor_nr = self.resource.query("XA")
+        return sensor_nr.split("A")[-1][0]
 
 
 def main():
-    itc = ITCDevice()
-    print(itc)
+    print("{}@sim".format(DEVPATH))
+    rm = visa.ResourceManager("{}@sim".format(DEVPATH))
+    itc01 = ITCDevice()
+    for resource in rm.list_resources():
+        print(resource)
+        if "GPIB" in resource:
+            itc01.set_resource(rm.open_resource)
 
-
+    print(itc01.auto_pid_on())
+    print(itc01.get_auto_heat_status())
 
 if __name__ == "__main__":
     main()
--- test/test_ITCDevice.py
+++ test/test_ITCDevice.py
@@ -1,22 +1,87 @@
 import unittest
 import visa
+import pyvisa
 
 from RunITC.ITCDevice import ITCDevice
+
+DEVPATH = '/home/chris/Programming/github/RunITC/test/devices.yaml'
 
 
 class DeviceTestCase(unittest.TestCase):
     """Test the device class."""
 
     def setUp(self):
-        self.rm = visa.ResourceManager('@sim')
-        itc01 = ITCDevice()
-        for resource in self.rm.list_resources():
-            if 'GPIB' in resource:
-                itc01.resource = self.rm(resource)
+        self.rm = visa.ResourceManager('{}@sim'.format(DEVPATH))
+        self.itc01 = ITCDevice()
+        for resource_address in self.rm.list_resources():
+            if 'GPIB' in resource_address:
+                self.itc01.set_resource(self.rm.open_resource)
 
-    def test_device_class_is_present(self):
-        """If something something."""
-        self.assertEqual(True, True)
+    def test_device_has_gpib_resource(self):
+        """Make sure the devices resource is a gpib resource."""
+        self.assertIsInstance(self.itc01.resource,
+                              pyvisa.resources.gpib.GPIBInstrument)
+
+    def test_device_read_termination_character(self):
+        "Make sure that the read termination character is a carriage return."
+        self.assertEqual(self.itc01.resource.read_termination, '\r')
+
+    def test_device_write_termination_character(self):
+        "Make sure that the write termination character is a carriage return."
+        self.assertEqual(self.itc01.resource.write_termination, '\r')
+
+    def test_itc_identity(self):
+        "Confirm the identity of the device"
+        self.assertEqual(self.itc01.resource.query('V'), "ITC503")
+
+    def test_read_tsorp(self):
+        "Test reading from sorption pump sensor"
+        self.assertEqual(self.itc01.get_tsorp(), "R249.200")
+
+    def test_read_t1k(self):
+        "Test reading from sorption pump sensor"
+        self.assertEqual(self.itc01.get_t1k(), "R7.000")
+
+    def test_read_the3(self):
+        "Test reading from sorption pump sensor"
+        self.assertEqual(self.itc01.get_the3(), "R7.000")
+
+    def test_set_heater_sensor(self):
+        "Test if setting the heater sensor to the sorption pump sensor worked"
+        self.itc01._set_heater_to_tsrop()
+        self.assertTrue(self.itc01.heater_set)
+        self.assertEqual(self.itc01.get_heater_sensor(), "1")
+
+    def test_get_heater_sensor(self):
+        "Test getting the heater sensor"
+        self.assertEqual(self.itc01.get_heater_sensor(), "1")
+
+    def test_set_tsorp_setpoint(self):
+        "Set the temperature the sorption pump should go to."
+        self.itc01.set_setpoint(30)
+        self.assertEqual(self.itc01.get_setpoint(), "R30.000")
+
+    def test_turn_on_auto_heat(self):
+        "Turn the auto heat control on."
+        self.itc01.auto_heat_on()
+        self.assertTrue(self.itc01.auto_heat)
+        self.assertEqual(self.itc01.get_auto_heat_status(), "1")
+
+    def test_turn_off_auto_heat(self):
+        "Turn the auto heat control off."
+        self.itc01.auto_heat_off()
+        self.assertFalse(self.itc01.auto_heat)
+        self.assertEqual(self.itc01.get_auto_heat_status(), "0")
+
+    def turn_on_auto_pid(self):
+        self.itc01.auto_pid_on()
+        self.assertTrue(self.itc01.auto_pid)
+        self.assertEqual(self.itc01.get_auto_pid_status(), "1")
+
+    def turn_off_auto_pid(self):
+        self.itc01.auto_pid_off()
+        self.assertFalse(self.itc01.auto_pid)
+        self.assertEqual(self.itc01.get_auto_pid_status(), "0")
 
 if __name__ == "__main__":
     unittest.main()
