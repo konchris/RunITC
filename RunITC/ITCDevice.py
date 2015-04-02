a2b3e30a7508aa9c6d572ea6df5f82ee93e43b8e
Christopher Espy <github@konchris.de>
2015-04-02 17:15:31 +0200

    Moved the tests and started using pyvisa-sim for mocking devices.

--- RunITC/ITCDevice.py
+++ RunITC/ITCDevice.py
@@ -1,7 +1,21 @@
 """The ITC Module.
 
 """
+import visa
 
 
-class ITCDevice:
-    pass
+class ITCDevice(object):
+
+    def __init__(self):
+        super(ITCDevice, self).__init__()
+        self.resource = None
+
+
+def main():
+    itc = ITCDevice()
+    print(itc)
+
+
+
+if __name__ == "__main__":
+    main()
