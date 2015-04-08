b44167373b29a83333991f482c4933ccc1bc061c
Christopher Espy <github@konchris.de>
2015-04-08 13:09:54 +0200

    Flushed out mock objects in buffer testing. Tested two devices. Changed the way device threads are started and stoped, as well as how collection threads are created, started, and stopped.

--- RunMeas/Buffer.py
+++ RunMeas/Buffer.py
@@ -9,46 +9,80 @@ import time
 from threading import Thread
 
 
+class BufferCollectionThread(Thread):
+
+    def __init__(self, name, q, delay=0.2):
+        super(BufferCollectionThread, self).__init__()
+        self.name = name
+        self.q = q
+        self.delay = delay
+        self.stop = False
+
+    def run(self):
+        while not self.stop:
+            vals = self.q.get()
+            print(self.name, vals)
+
+    def stop_thread(self):
+        self.stop = True
+
+
 class Buffer(object):
 
     def __init__(self, devices):
-        self.devices = self._generate_device_dictionary(devices)
-
-    def _generate_device_dictionary(self, devices):
         if not isinstance(devices, list):
             raise TypeError("The devices passed to the manager needs to be a "
                             "list of tuples")
         if not isinstance(devices[0], tuple):
             raise TypeError("Each unit of the devices list need to be a tuple")
 
+        self.devices = self._generate_device_dictionary(devices)
+        self.collection_threads = self._generate_collection_threads()
+
+    def _generate_device_dictionary(self, devices):
         d = {}
 
+        for dev in devices:
+            (dev_name, dev_obj, dev_thread, dev_queue) = dev
+            d[dev_name] = {'device': dev_obj, 'thread': dev_thread,
+                           'queue': dev_queue}
+
+        return d
+
+    def _generate_collection_threads(self):
+        col_ts = []
+        for dev_name, dev_obj in self.devices.items():
+            t = BufferCollectionThread(dev_name, dev_obj['queue'])
+            col_ts.append(t)
+        return col_ts
+
     def start_collection(self):
-        for k, v in self.queues.items():
-            print('Starting queue: {}'.format(k))
+        # Make sure that all the device threads are started
+        for k, v in self.devices.items():
+            print('Starting device thread: {}'.format(k))
+            if not v['thread'].is_alive():
+                v['thread'].start()
 
-        while not self.stop:
-            print('Stop:', self.stop)
-            for k, v in self.queues.items():
-                print('Queue: {qname}, Value: {val}'.format(qname=k,
-                                                            val=v.get()))
+        for t in self.collection_threads:
+            print('Starting', t.name)
+            t.start()
 
-        for k, v in self.queues.items():
-            print('Stopping queue: {}'.format(k))
-            v.task_done()
+    def stop_collection(self):
+        for t in self.collection_threads:
+            print('Stopping', t.name)
+            t.stop_thread()
+            t.join()
 
-    def stop_thread(self):
-        self.stop = True
-
-
-class BufferCollectionThread(Thread):
-    pass
+    def stop_all_devices(self):
+        for k, v in self.devices.items():
+            if v['thread'].is_alive():
+                print('Stopping device thread: {}'.format(k))
+                v['thread'].stop_thread()
 
 
 def main():
 
     import visa
-    from threading import Thread
     from queue import Queue
 
     from ITCDevice import ITCDevice, ITCMeasurementThread
@@ -56,28 +90,28 @@ def main():
     DEVPATH = '/home/chris/Programming/github/RunMeas/test/devices.yaml'
 
     rm = visa.ResourceManager("{}@sim".format(DEVPATH))
-    itc01 = ITCDevice()
     for resource in rm.list_resources():
-        if "GPIB" in resource:
+        if "GPIB" in resource and '24' in resource:
+            itc01 = ITCDevice(address=resource)
             itc01.set_resource(rm.open_resource)
+        elif 'GPIB' in resource and '23' in resource:
+            itc02 = ITCDevice(address=resource)
+            itc02.set_resource(rm.open_resource)
 
-    itc_queue = Queue()
-    my_buffer = Buffer({'ITC Queue': itc_queue})
+    print(itc01.address)
+    print(itc02.address)
 
-    itc_thread = ITCMeasurementThread(itc01, itc_queue, delay=0.1)
-    print('starting thread')
-    itc_thread.start()
-    t = Thread(target=my_buffer.start_collection)
-    t.start()
-    print('Is t alive, start?:', t.is_alive())
+    itc01_queue = Queue()
+    itc02_queue = Queue()
 
+    itc01_thread = ITCMeasurementThread(itc01, itc01_queue, delay=0.1)
+    itc02_thread = ITCMeasurementThread(itc02, itc02_queue, delay=0.2)
+    my_buffer = Buffer([('ITC1 Queue', itc01, itc01_thread, itc01_queue),
+                        ('ITC2 Queue', itc02, itc02_thread, itc02_queue)])
+    my_buffer.start_collection()
     time.sleep(1)
-    my_buffer.stop_thread()
-    itc_thread.stop_thread()
-    t.join()
-    print('Is t alive after stop?', t.is_alive())
-    print('Exiting')
-
+    my_buffer.stop_collection()
+    my_buffer.stop_all_devices()
 
 if __name__ == "__main__":
     main()
--- test/test_Buffer.py
+++ test/test_Buffer.py
@@ -1,50 +1,175 @@
 import unittest
 
-import visa
 import time
+from datetime import datetime
 from queue import Queue
+from threading import Thread
 
 from RunMeas.Buffer import Buffer
-from RunMeas.ITCDevice import ITCDevice, ITCMeasurementThread
-
-DEVPATH = '/home/chris/Programming/github/RunMeas/test/devices.yaml'
 
 
-class DeviceTestCase(unittest.TestCase):
+class MockResource(object):
+
+    def __init__(self, address):
+        self.address = address
+
+
+class MockDevice(object):
+
+    def __init__(self, address):
+        self.resource = None
+        self.address = address
+
+    def set_resource(self, resource):
+        self.resource = resource(self.address)
+
+    def get_values(self):
+        now = datetime.now()
+        return (now, 42)
+
+
+class MockDeviceMeasurementThread(Thread):
+
+    def __init__(self, device, q, delay=0.1):
+        super(MockDeviceMeasurementThread, self).__init__()
+        self.stop = False
+        self.device = device
+        self.q = q
+        self.delay = delay
+
+    def run(self):
+        while not self.stop:
+            time.sleep(self.delay)
+            meas_values = self.device.get_values()
+            self.q.put(meas_values)
+
+    def stop_thread(self):
+        self.stop = True
+
+
+class BufferTestCase(unittest.TestCase):
     """Test the device class."""
 
     def setUp(self):
-        self.rm = visa.ResourceManager('{}@sim'.format(DEVPATH))
-        self.itc01 = ITCDevice()
-        for resource_address in self.rm.list_resources():
-            if 'GPIB' in resource_address:
-                self.itc01.set_resource(self.rm.open_resource)
+        self.delay = 0.01
 
-        self.itc_queue = Queue()
+        self.itc01 = MockDevice('1')
+        self.itc01.set_resource(MockResource)
 
-        self.delay = 0.1
+        self.itc01_queue = Queue()
 
-        self.itc_thread = ITCMeasurementThread(self.itc01, self.itc_queue,
-                                               delay=self.delay)
-        self.buffer = Buffer({'ITC Queue': self.itc_queue})
+        self.itc01_thread = MockDeviceMeasurementThread(self.itc01,
+                                                        self.itc01_queue,
+                                                        delay=self.delay)
 
-        self.itc_thread.start()
+        self.itc02 = MockDevice('2')
+        self.itc02.set_resource(MockResource)
 
-    def test_buffer_gets_queue(self):
-        """If something something."""
-        for k, v in self.buffer.queues.items():
-            self.assertIsInstance(v, Queue)
+        self.itc02_queue = Queue()
 
-    def test_buffer_has_test_queue(self):
-        self.assertTrue('ITC Queue' in self.buffer.queues)
+        self.itc02_thread = MockDeviceMeasurementThread(self.itc02,
+                                                        self.itc02_queue,
+                                                        delay=self.delay)
+
+        self.buffer = Buffer([('Mock Device 01', self.itc01, self.itc01_thread,
+                               self.itc01_queue),
+                              ('Mock Device 02', self.itc02, self.itc02_thread,
+                               self.itc02_queue)])
+
+        self.itc01_thread.start()
+        self.itc02_thread.start()
+
+    def test_buffer_init_creates_devices_dictionary(self):
+        delay = 0.01
+
+        itc01 = MockDevice('1')
+        itc01.set_resource(MockResource)
+
+        itc01_queue = Queue()
+
+        itc01_thread = MockDeviceMeasurementThread(itc01,
+                                                   itc01_queue,
+                                                   delay=delay)
+
+        my_buffer = Buffer([('Mock Device 01', itc01, itc01_thread,
+                             itc01_queue)
+                            ])
+        self.assertIsInstance(my_buffer.devices, dict)
+        self.assertIn('Mock Device 01', my_buffer.devices)
+
+        self.assertIsInstance(my_buffer.devices['Mock Device 01'], dict)
+
+        self.assertIn('queue', my_buffer.devices['Mock Device 01'])
+        self.assertIsInstance(my_buffer.devices['Mock Device 01']['queue'],
+                              Queue)
+
+        self.assertIn('device', my_buffer.devices['Mock Device 01'])
+        self.assertIsInstance(my_buffer.devices['Mock Device 01']['device'],
+                              object)
+
+        self.assertIn('thread', my_buffer.devices['Mock Device 01'])
+        self.assertIsInstance(my_buffer.devices['Mock Device 01']['thread'],
+                              Thread)
+
+    def test_buffer_init_exception_no_list(self):
+        with self.assertRaises(TypeError):
+            Buffer('Hello World')
+
+    def test_buffer_init_exception_no_tuples(self):
+        with self.assertRaises(TypeError):
+            Buffer(['0123', '4567'])
+
+    def test_buffer_init_creates_collection_threads(self):
+        delay = 0.01
+
+        itc01 = MockDevice('1')
+        itc01.set_resource(MockResource)
+
+        itc01_queue = Queue()
+
+        itc01_thread = MockDeviceMeasurementThread(itc01,
+                                                   itc01_queue,
+                                                   delay=delay)
+
+        my_buffer = Buffer([('Mock Device 01', itc01, itc01_thread,
+                             itc01_queue)
+                            ])
+        self.assertIsInstance(my_buffer.collection_threads, list)
+        self.assertTrue(my_buffer.collection_threads)
+
+    def test_buffer_has_mock_device(self):
+        self.assertTrue('Mock Device 01' in self.buffer.devices.keys())
+        self.assertTrue('Mock Device 02' in self.buffer.devices.keys())
+        self.assertTrue('device' in
+                        self.buffer.devices['Mock Device 01'].keys())
+        self.assertTrue(self.buffer.devices['Mock Device 01']['device']
+                        is not None)
+        self.assertTrue('queue' in
+                        self.buffer.devices['Mock Device 01'].keys())
+        self.assertTrue(self.buffer.devices['Mock Device 01']['queue']
+                        is not None)
+        self.assertTrue('thread' in
+                        self.buffer.devices['Mock Device 01'].keys())
+        self.assertTrue(self.buffer.devices['Mock Device 01']['thread']
+                        is not None)
 
     def test_buffer_start_stop_queue(self):
         self.buffer.start_collection()
-        time.sleep(0.5)
-        self.Buffer.stop()
+        time.sleep(0.1)
+        self.buffer.stop_collection()
+
+    def test_buffer_stop_all_devices(self):
+        self.assertTrue(self.itc01_thread.is_alive())
+        self.buffer.stop_all_devices()
+        for dev_name, dev_dict in self.buffer.devices.items():
+            dev_dict['thread'].join()
+            self.assertFalse(dev_dict['thread'].is_alive())
 
     def tearDown(self):
-        self.itc_thread.stop_thread()
+        if self.itc01_thread.is_alive():
+            self.itc01_thread.stop_thread()
+        if self.itc02_thread.is_alive():
+            self.itc02_thread.stop_thread()
 
 if __name__ == "__main__":
     unittest.main()
