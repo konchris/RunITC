b34722ad9fb346d132ed01a023821621af5e9c32
Christopher Espy <github@konchris.de>
2015-04-30 12:21:51 +0200

    Added and tested more functionality to AHDevice

--- RunMeas/AHDevice.py
+++ RunMeas/AHDevice.py
@@ -21,8 +21,102 @@ from queue import Queue
 
 
 class AHDevice(object):
-    """The AH Driver Object"""
-    pass
+    """The AH Driver Object
+
+    This provides high level access to some of the functionality of the AH2550A
+    capacitive bridge.
+    The device is directly accessable via the resource attribute.
+
+    Parameters
+    ----------
+    address : str
+        The visa address of the ITC device.
+        Example: "GPIB1::24::INSTR"
+    read_term : str, optional
+        The reading terminatin character of the device
+        DEFUALT: "\n"
+
+    Attributes
+    ----------
+    resource : pyvisa.resources.gpib.GPIBInstrument
+        The instance of the gpib instrument giving direct access to the device.
+    address : str
+        The visa address of the device
+        Example: "GPIB1::24::INSTR"
+    read_term : str
+        The reading termination character of the device
+    write_term : str
+        The writing termination character of the device
+
+    Methods
+    -------
+    set_resource(resource=, resource_address)
+
+    """
+
+    def __init__(self, address, read_term="\n",
+                 write_term="\n"):
+        super(AHDevice, self).__init__()
+        self.resource = None
+        self.address = address
+        self.read_term = read_term
+        self.write_term = write_term
+
+    def set_resource(self, resource):
+        """Set the VISA resource for the device.
+
+        The sets the VISA resources as the 'resource' attribute of the object.
+
+        Parameters
+        ----------
+        resource : method
+            This is the method from
+            pyvisa.highlevel.ResourceManager.open_resource() and it will take
+            the subsequent address attribute of the class as a parameter.
+
+        """
+        self.resource = resource(self.address,
+                                 read_termination=self.read_term,
+                                 write_termination=self.write_term)
+
+    def get_average(self):
+        """Get the approximate time used to make a measurement.
+
+        Returns
+        -------
+        tuple : (str, int)
+            A tupe with the name of the value ('AVERAGE') and the value.
+
+        """
+        aveg_rsp = self.resource.query("SH AV")
+        aveg_exp = int(aveg_rsp.split('=')[-1])
+        print(aveg_exp, type(aveg_exp))
+        return('AVERAGE', aveg_exp)
+
+    def set_average(self, aveg_exp):
+        """Set the approximate time usedto make a measurement
+
+        Parameters
+        ----------
+        aveg_exp : int
+            The time averaging exponent
+
+        """
+        if not isinstance(aveg_exp, int):
+            raise TypeError("The averaging exponent provided needs to be an "
+                            "int.")
+        self.resource.write("AV {:d}".format(aveg_exp))
+
+    def start_continuous(self):
+        "Initiate measurements which are taken continuously"
+        self.resource.write('CO ON')
+
+    def stop_continuous(self):
+        "Stop measurements which are taken continuously"
+        self.resource.write('CO OF')
+
+    def get_values(self):
+        "Hi"
 
 
 class AHMeasurementThread(Thread):
--- test/test_AHDevice.py
+++ test/test_AHDevice.py
@@ -10,3 +10,62 @@ from RunMeas.AHDevice import AHDevice, AHMeasurementThread
 
 DEVPATH = os.path.join(os.getcwd(), 'test', 'devices.yaml')
 # DEVPATH = '/home/chris/Programming/github/RunMeas/test/devices.yaml'
+
+class DeviceTestCase(unittest.TestCase):
+    """Test the device class."""
+
+    def setUp(self):
+        rm = visa.ResourceManager('{}@sim'.format(DEVPATH))
+        for resource_address in rm.list_resources():
+            if 'GPIB' in resource_address and '28' in resource_address:
+                self.ah = AHDevice(resource_address)
+                self.ah.set_resource(rm.open_resource)
+
+    def test_create_ah_device(self):
+        rm = visa.ResourceManager('{}@sim'.format(DEVPATH))
+        for resource_address in rm.list_resources():
+            print(resource_address)
+            if 'GPIB' in resource_address and '28' in resource_address:
+                ah = AHDevice(resource_address)
+                ah.set_resource(rm.open_resource)
+        self.assertIsInstance(ah.resource,
+                              pyvisa.resources.gpib.GPIBInstrument)
+
+    def test_device_has_gpib_resource(self):
+        """Make sure the devices resource is a gpib resource."""
+        self.assertIsInstance(self.ah.resource,
+                              pyvisa.resources.gpib.GPIBInstrument)
+
+    def test_device_read_termination_character(self):
+        "Make sure that the read termination character is a line feed."
+        self.assertEqual(self.ah.resource.read_termination, '\n')
+
+    def test_device_write_termination_character(self):
+        "Make sure that the write termination character is a line feed."
+        self.assertEqual(self.ah.resource.write_termination, '\n')
+
+    def test_ah_identity(self):
+        "Confirm the identity of the device"
+        self.assertEqual(self.ah.resource.query('*IDN?'), "AH2550A")
+
+    def test_read_average_exponent(self):
+        "Test reading the average exponent"
+        self.assertEqual(self.ah.get_average(), ('AVERAGE', 4))
+
+    def test_set_average_exponent(self):
+        "Test setting the averaging exponent"
+        self.ah.set_average(5)
+        # The following 'read' command is to trick the PyVISA-sim device,
+        # which does not allow for write commands without a paired read
+        # command.
+        self.ah.resource.read()
+        self.assertEqual(self.ah.get_average(), ('AVERAGE', 5))
+
+    def test_start_continuous(self):
+        "Test starting continuous measurements"
+        self.ah.start_continuous()
+
+    def test_stop_continuous(self):
+        "Test starting continuous measurements"
+        self.ah.stop_continuous()
+        
