b438fb98cd60925674d477f1d7602fd815cf4ac8
Christopher Espy <bitbucket@konchris.de>
2015-05-20 15:38:34 +0200

    Removed testing of AHMeasurementThread as there is no object of that name yet.

--- test/test_AHDevice.py
+++ test/test_AHDevice.py
@@ -7,7 +7,7 @@ from queue import Queue
 from datetime import datetime
 
 
-from RunMeas.AHDevice import AHDevice, AHMeasurementThread
+from RunMeas.AHDevice import AHDevice  # , AHMeasurementThread
 
 DEVPATH = os.path.join(os.getcwd(), 'test', 'devices.yaml')
 # DEVPATH = '/home/chris/Programming/github/RunMeas/test/devices.yaml'
